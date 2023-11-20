0c21c16062a27139f18f24fc4353408ee1bda8b8
Alena Kuimova <alvikui@yandex.ru>
2023-11-20 16:25:24 +0300

    Third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	let filterGender = users.filter(user => user.gender === gender).map(user => user.age);
+	let result = filterGender.reduce((acc, arr) => acc + arr, 0) / filterGender.length;
+
+	return result > 0 ? result : 0;
 }
